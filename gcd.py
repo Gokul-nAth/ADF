0cbbb55a5f2d3397ef3732098c28bd761a0b8aae
Gokul-nAth <77395399+Gokul-nAth@users.noreply.github.com>
2021-06-19 23:12:19 +0530

    update

--- gcd.py
+++ gcd.py
@@ -5,7 +5,7 @@ try:
         smallest=a
         if(b<a):
             smallest=b
-        for i in range(b,0,-1):
+        for i in range(smallest,0,-1):
                 if(a%i==0 and b%i==0):
                     print('GCD of given numbers : ',i)
                     break
--- main.py
+++ main.py
@@ -1,3 +1,3 @@
-import ascii
+import gcd
 if(__name__ == '__main__'):
     print()
