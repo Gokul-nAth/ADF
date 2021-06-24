6c7549d33214908d80e32e5126f647f314f5562e
Gokul-nAth <77395399+Gokul-nAth@users.noreply.github.com>
2021-06-24 16:05:11 +0530

    day 4

--- Day 2/day2.py
+++ Day 2/day2.py
@@ -1,42 +1,56 @@
+"""Example of pylint"""
 import sys
 import re
 from collections import Counter
 
+
 try:
-    #iFile=sys.argv[1]
-    iFile='day2.txt'
-    inputFileList=open(iFile,'r').read().split(' ')
-    if(len(inputFileList)!=0):
-        to=0
-        ing=0
+    IFILE=sys.argv[1]
+    #IFILE='day2.txt'
+    iFileList=open(IFILE,'r').read().split(' ')
+    inputFileList=[]
+    for word in iFileList:
+        if '.' in word:
+            inputFileList.append(word[:word.index('.')])
+            if word.index('.')+1<len(word):
+                inputFileList.append(word[word.index('.')+1:])
+        elif ',' in word:
+            inputFileList.append(word[:word.index(',')])
+            inputFileList.append(word[word.index(',') + 1:])
+        elif '\n' in word:
+            inputFileList.append(word[:word.index('\n')])
+            inputFileList.append(word[word.index('\n')+1:])
+        else:
+            inputFileList.append(word)
+    if len(inputFileList)!=0:
+        TO=0
+        ING=0
         palindrome=[]
         uniqueList=[]
         for word in inputFileList:
-            if(word[:2]=='To' or word[:2]=='to'):
-                to+=1
-            if(word[-3:]=='ing'):
-                ing+=1
-            if(word.lower()==word[::-1].lower()):
+            if word[:2]=='To' or word[:2]=='to':
+                TO+=1
+            if word[-3:]=='ing':
+                ING+=1
+            if word.lower()==word[::-1].lower():
                 palindrome.append(word)
 
-        print("Number of words whose prefix is To/to : ",to,'\n')
-        print('Number of words whose suffix is "ing" : ',ing,'\n')
-        dict=Counter(inputFileList)
-        max=0
-        for key in dict:
-            if(dict[key]>max):
-                max=dict[key]
+        print('Number of words whose prefix is To/to : ',TO,'\n')
+        print('Number of words whose suffix is "ing" : ',ING,'\n')
+
+        dictC=Counter(inputFileList)
+        MAXI=0
+        maxList=[]
+        for key in dictC:
+            if dictC[key]>MAXI:
+                MAXI=dictC[key]
         print('Word(s) with maximum number of occurance is : ',end=" ")
-        fisrtWord=True
-        for key in dict:
+        for key in dictC:
             uniqueList.append(key)
-            if(fisrtWord and dict[key]==max):
-                print(key,end="")
-                fisrtWord=False
-                continue
-            if(dict[key]==max):
-                print(','+key,end="")
-        print('\n\nPalindrome word(s) : '+str(palindrome)[1:-1])
+            if dictC[key]==MAXI:
+                maxList.append(key)
+        print(maxList)
+        print('\nPalindrome word(s) : ',palindrome)
         print('\nUnique list : ',uniqueList)
         Word={}
         i=1
@@ -44,36 +58,48 @@ try:
             Word[i]=word
             i+=1
         print('\nDictionary : ',Word)
-        file='file'
+
+
+
+
+        FILE='file'
         for i in range(5):
-            openFile=open(file+str(i+1)+'.txt','a')
-            j=-1
-            for word in inputFileList:
-                j+=1
-                if(i==0):
-                    value=re.split('a|e|i|o|u',word)
-                    for letter in value:
-                        openFile.write(letter+' ')
-                elif(i==1):
-                    try:
-                        openFile.write(word[:2]+word[2].upper()+word[3:])
-                    except IndexError:
+            with open(FILE+str(i+1)+'.txt','w') as openFile:
+                j=-1
+                if i == 4:
+                    iFileList = open(IFILE, 'r').read().split(' ')
+                    for w in iFileList:
+                        if '\n' in w:
+                            openFile.write(w[:w.index('\n')] + ';\n' + w[w.index('\n') + 1:] + ' ')
+                            continue
+                        openFile.write(w + ' ')
+                for word in inputFileList:
+                    j+=1
+                    if i==0:
+                        value=re.split('a|e|i|o|u',word)
+                        for letter in value:
+                            openFile.write(letter+' ')
+                    elif i==1:
+                        try:
+                            openFile.write(word[:2]+word[2].upper()+word[3:])
+                        except IndexError:
+                            openFile.write(word)
+                    elif i==2:
+                        if j==4:
+                            openFile.write(word.upper()+' ')
+                            continue
                         openFile.write(word)
-                elif(i==2):
-                    if(j==4):
-                        openFile.write(word.upper()+' ')
-                        continue
-                    openFile.write(word)
-                elif(i==3):
-                    if(j==0):
-                        openFile.write(word)
-                        continue
-                    else:
-                        openFile.write('-'+word)
-                        continue
-                openFile.write(' ')
-            openFile.close()
+                    if i!=3:
+                        openFile.write(' ')
+                    if i==3:
+                        if j==0:
+                            openFile.write(word)
+                        else:
+                            openFile.write('-'+word)
+
     else:
         print('The given file is empty')
 except ValueError:
     print('Unable to open given file')
+except IndexError:
+    print('No input')
